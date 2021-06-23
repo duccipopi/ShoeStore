c67f1dc2abcbf411f636f4f46747aa7182eb56a7
Ricardo Ducci Popi <ducci.popi@gmail.com>
2021-06-22 23:22:55 -0300

    Initial commit

---
+++ app/src/main/java/com/udacity/shoestore/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.udacity.shoestore
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.udacity.shoestore.databinding.ActivityMainBinding
+import timber.log.Timber
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Timber.plant(Timber.DebugTree())
+
+        val binding = DataBindingUtil.setContentView<ActivityMainBinding>(this, R.layout.activity_main)
+
+
+    }
+}
---
+++ app/src/main/java/com/udacity/shoestore/screens/login/LoginFragment.kt
@@ -0,0 +1,38 @@
+package com.udacity.shoestore.screens.login
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.databinding.DataBindingUtil
+import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
+import androidx.navigation.ui.NavigationUI
+import com.udacity.shoestore.R
+import com.udacity.shoestore.databinding.LoginFragmentBinding
+
+/**
+ * Fragment to Login page
+ */
+class LoginFragment : Fragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        val binding: LoginFragmentBinding =
+            DataBindingUtil.inflate(inflater, R.layout.login_fragment, container, false)
+
+        binding.loginButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment) }
+
+        binding.registerButton.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_welcomeFragment)
+        }
+
+        // Inflate the layout for this fragment
+        return binding.root
+    }
+
+}
